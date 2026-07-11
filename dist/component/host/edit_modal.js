e82913d4cf6dcf66a55151be55dc9c3f40a3b66d
Lord <76169834+lordn16@users.noreply.github.com>
2026-07-11 12:57:01 +0200

    style

--- dist/component/host/edit_modal.js
+++ dist/component/host/edit_modal.js
@@ -8,7 +8,7 @@ export class EditDeviceModal extends FormModal {
         this.header = document.createElement("h2");
         const i = getTranslations(getCurrentLanguage()).host;
         this.header.innerText = i.editDeviceHeader;
-        this.displayName = new InputComponent("displayName", "text", i.displayName, {
+                this.displayName = new InputComponent("displayName", "text", i.displayName, {
             value: initialValues.displayName || ""
         });
         this.iconUrl = new InputComponent("iconUrl", "text", i.iconUrl, {
@@ -18,6 +18,10 @@ export class EditDeviceModal extends FormModal {
         this.info = new InputComponent("info", "text", i.info, {
             value: initialValues.info || ""
         });
+        this.statusApiUrl = new InputComponent("statusApiUrl", "text", i.statusApiUrl || "Status API URL", {
+            value: initialValues.statusApiUrl || "",
+            placeholer: i.statusApiUrlHelp || "URL to check online/offline and uptime status"
+        });
         this.bootApiUrl = new InputComponent("bootApiUrl", "text", i.bootApiUrl || "Boot API URL", {
             value: initialValues.bootApiUrl || "",
             placeholer: i.bootApiUrlHelp || "URL to trigger boot/power state"
@@ -25,10 +29,6 @@ export class EditDeviceModal extends FormModal {
         this.shutdownApiUrl = new InputComponent("shutdownApiUrl", "text", i.shutdownApiUrl || "Force Off API URL", {
             value: initialValues.shutdownApiUrl || "",
             placeholer: i.shutdownApiUrlHelp || "URL to force shutdown"
-        });
-        this.statusApiUrl = new InputComponent("statusApiUrl", "text", i.statusApiUrl || "Status API URL", {
-            value: initialValues.statusApiUrl || "",
-            placeholer: i.statusApiUrlHelp || "URL to check online/offline and uptime status"
         });
         this.f7ApiUrl = new InputComponent("f7ApiUrl", "text", i.f7ApiUrl || "F7 Toggle API URL", {
             value: initialValues.f7ApiUrl || "",
@@ -40,9 +40,9 @@ export class EditDeviceModal extends FormModal {
         this.displayName.setValue((_a = this.initialValues.displayName) !== null && _a !== void 0 ? _a : "");
         this.iconUrl.setValue((_b = this.initialValues.iconUrl) !== null && _b !== void 0 ? _b : "");
         this.info.setValue((_c = this.initialValues.info) !== null && _c !== void 0 ? _c : "");
-        this.bootApiUrl.setValue((_d = this.initialValues.bootApiUrl) !== null && _d !== void 0 ? _d : "");
-        this.shutdownApiUrl.setValue((_e = this.initialValues.shutdownApiUrl) !== null && _e !== void 0 ? _e : "");
-        this.statusApiUrl.setValue((_f = this.initialValues.statusApiUrl) !== null && _f !== void 0 ? _f : "");
+        this.statusApiUrl.setValue((_d = this.initialValues.statusApiUrl) !== null && _d !== void 0 ? _d : "");
+        this.bootApiUrl.setValue((_e = this.initialValues.bootApiUrl) !== null && _e !== void 0 ? _e : "");
+        this.shutdownApiUrl.setValue((_f = this.initialValues.shutdownApiUrl) !== null && _f !== void 0 ? _f : "");
         this.f7ApiUrl.setValue((_g = this.initialValues.f7ApiUrl) !== null && _g !== void 0 ? _g : "");
     }
     submit() {
@@ -50,9 +50,9 @@ export class EditDeviceModal extends FormModal {
             displayName: this.displayName.getValue(),
             iconUrl: this.iconUrl.getValue(),
             info: this.info.getValue(),
+            statusApiUrl: this.statusApiUrl.getValue(),
             bootApiUrl: this.bootApiUrl.getValue(),
             shutdownApiUrl: this.shutdownApiUrl.getValue(),
-            statusApiUrl: this.statusApiUrl.getValue(),
             f7ApiUrl: this.f7ApiUrl.getValue()
         };
     }
@@ -61,9 +61,9 @@ export class EditDeviceModal extends FormModal {
         this.displayName.mount(form);
         this.iconUrl.mount(form);
         this.info.mount(form);
+        this.statusApiUrl.mount(form);
         this.bootApiUrl.mount(form);
         this.shutdownApiUrl.mount(form);
-        this.statusApiUrl.mount(form);
         this.f7ApiUrl.mount(form);
     }
 }
